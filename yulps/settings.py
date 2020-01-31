3cfa6d917a9ea94f4dd7363d3aabb00eabe909d2
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-01-30 21:38:43 -0600

    Se agrega Sass al proyecto.

--- yulps/settings.py
+++ yulps/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3th libs
+    'sass_processor',
+
     # Local apps
     'users'
 ]
@@ -130,3 +133,15 @@ STATICFILES_DIRS = [
 
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
+
+# Sass configuration
+# Set the statics files
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r'^.+\.scss$'
