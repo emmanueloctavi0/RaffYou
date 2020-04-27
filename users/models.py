1085d160f307343e0235390e7cc1bb96616d3773
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-04-26 19:10:03 -0500

    The user models was modified. Added facebook_id, the username is blank and added a custom user manager.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email is required')
+    
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -10,5 +30,30 @@ class User(AbstractUser):
         unique=True
     )
 
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        blank=True,
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+
+    facebook_id = models.CharField(
+        _('ID de Facebook'),
+        max_length=250,
+        unique=True,
+        null=True
+    )
+
+    profile_picture = models.ImageField(
+        _('Foto de perfil'),
+        upload_to='images/users',
+        blank=True
+    )
+
+    objects = CustomUserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
