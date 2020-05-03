577fa05a9a29e9b837865ad6272a7c2194910828
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-05-03 16:26:33 -0500

    Filter in search added.

--- products/views/products_home_view.py
+++ products/views/products_home_view.py
@@ -2,6 +2,8 @@
 # Django
 from django.views.generic import ListView
 from django.shortcuts import redirect
+from django.db.models import Q
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Models
 from products.models import Product
@@ -11,6 +13,20 @@ class ProductsHomeView(ListView):
     template_name = 'products/home.html'
     model = Product
     paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        try:
+            query = self.request.GET['q']
+        except MultiValueDictKeyError:
+            return queryset
+
+        return queryset.filter(
+            Q(name__icontains=query) |
+            Q(tags__value__icontains=query) |
+            Q(tags__description__icontains=query) |
+            Q(description__icontains=query)
+        )
 
 
 def redirect_home_view(request):
