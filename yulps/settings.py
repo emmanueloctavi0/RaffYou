0339871032a20b6e754177ede44b21fcee218c0c
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-05-10 23:59:25 -0500

    Configurate settings for database.

--- yulps/settings.py
+++ yulps/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'yulps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': '',
     }
 }
 
@@ -136,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
