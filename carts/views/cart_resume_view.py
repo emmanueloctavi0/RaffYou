7aa7c9ec5a5627b04ccacfd80f160e8a2c5eee16
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-05-17 21:54:24 -0500

    Cart resume template finished.

--- carts/views/cart_resume_view.py
+++ carts/views/cart_resume_view.py
@@ -8,4 +8,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class CartResumeView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'carts/cart_resume.html')
+        cart_products = request.user.cart.cartproduct_set.all()
+        cart = request.user.cart
+
+        return render(
+            request,
+            'carts/cart_resume.html',
+            context={
+                'cart_products': cart_products,
+                'cart': cart,
+            }
+        )
