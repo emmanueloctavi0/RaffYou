66cb3ee71baf54a145fd5add9f5012120f5a8511
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-05-28 19:34:47 -0500

    Fixtures updated.

--- loaddata.sh
+++ loaddata.sh
@@ -1,4 +1,5 @@
 python manage.py loaddata users.json
 python manage.py loaddata tags.json
 python manage.py loaddata providers.json
+python manage.py loaddata provider_address.json
 python manage.py loaddata products.json
