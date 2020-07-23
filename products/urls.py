25901ed15420a17dc8d8597b27d06bc1d7e67800
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2020-07-23 13:46:35 -0500

    Providers added in a section.

--- products/urls.py
+++ products/urls.py
@@ -10,6 +10,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.ProductsHomeView.as_view(), name='home'),
+    path('negocios/', views.ProviderListView.as_view(), name='provider-list'),
     path('negocio/<int:pk>/', views.ProviderDetailView.as_view(), name='provider-detail'),
     path('<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('pedir/<int:product>/<number>/', views.wa_view, name='wa_view'),
