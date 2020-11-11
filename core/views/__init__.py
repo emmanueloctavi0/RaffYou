c5d58b3f107e5fc470ad7903424b314ec65dcae0
Javier Silva <chavi.sennin@gmail.com>
2020-11-11 12:55:56 -0600

    Se agrego la landing page, falta diseñarla.

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,3 @@
 from core.views.redirect_home_view import redirect_home_view
 from core.views.privacy_policy_view import PrivacyPolicyView, TermsAndConditionsView
+from core.views.landing_page_view import LandingPageView
